31cdbfd41a40d39a9c3218aa5b4174d229941501
Cameron Zheng <cameronzheng2002@gmail.com>
2025-09-07 10:05:02 -0700

    first commit

---
+++ snake.py
@@ -0,0 +1,51 @@
+import pygame
+
+# 10x10 grid of snake
+
+# pygame setup
+pygame.init()
+screen_width = 720
+screen_height = 720
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+running = True
+
+
+
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+while running:
+  # poll for eventws 
+  # pygame.QUIT event means teh user clicked X to close your window
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+    
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("green")
+
+    # RENDER GAME HERE
+    # border of the game
+    top_left_point = [15, 15]
+    top_right_point = [screen_width - 15, 15]
+    bot_left_point = [15, screen_height - 15]
+    bot_right_point = [screen_width - 15, screen_height - 15]
+
+    pygame.draw.line(screen, "black", top_left_point, top_right_point, width = 2)  # top border
+    pygame.draw.line(screen, "black", bot_left_point, bot_right_point, width = 2)  # bottom border
+    pygame.draw.line(screen, "black", top_left_point, bot_left_point, width = 2)  # left border
+    pygame.draw.line(screen, "black", top_right_point, bot_right_point, width = 2)  # right border
+    
+    pygame.draw.rect(screen, "blue", (500, 50, 50, 50))
+
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-independent physics
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
